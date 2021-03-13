c705a402b35cc9b7aca0c2fab7f041ae04d8a55d
Aziz <mohamedazizkhayati@insat.u-carthage.tn>
2021-03-13 15:37:10 +0100

    second commit

--- List Maker/Scripts/main.js
+++ List Maker/Scripts/main.js
@@ -1,11 +1,22 @@
 todo = document.querySelector("#ToDo");
 todo.addEventListener('click', submit, false);
+
 function submit(event) {
     i1 = document.querySelector("#input1");
     i2 = document.querySelector("#input2");
-    var tag = document.createElement("p");
-    var text = document.createTextNode(i1.value+":"+i2.value);
+    var tag = document.createElement("li");
+    var text = document.createTextNode(i1.value+" : "+i2.value+ " ");
     tag.appendChild(text);
-    var element = document.querySelector("td");
+    tag.innerHTML += '<img src="Assets/trash.svg" alt="">';
+    console.log(tag);
+    tag.className += "list-group-item d-flex justify-content-between align-items-center";
+    var element = document.querySelector("ul");
     element.appendChild(tag);
+}
+body = document.querySelector("body");
+body.addEventListener("click", onClick, false);
+function onClick(event) {
+    if(event.target.tagName == 'IMG'){
+        event.target.parentNode.remove();
+    }
 }
